668012c0dc2c66f4d6ea85c001471b17445c84de
wmw04087 <wmw04087@gmail.com>
2025-06-18 02:21:52 +0900

    refactor: 시도 횟수 RaceCount로 책임 분리

---
+++ src/main/java/racingcar/model/RaceCount.java
@@ -0,0 +1,20 @@
+package racingcar.model;
+
+public class RaceCount {
+
+    private static final String RACE_COUNT_ERROR_MESSAGE = "이동 횟수는 1이상이어야 합니다.";
+    private static final int MINIMUM_COUNT = 1;
+
+    private final int count;
+
+    public RaceCount(int count) {
+        validateCount(count);
+        this.count = count;
+    }
+
+    private void validateCount(int count) {
+        if (count < MINIMUM_COUNT) {
+            throw new IllegalArgumentException(RACE_COUNT_ERROR_MESSAGE);
+        }
+    }
+}
